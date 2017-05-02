7ee55ef78759b90524837614e5b322da6b6a2681
Evaldas Ruseckas <evaldasr@lt.ibm.com>
2017-05-02 17:04:48 +0300

    improve code quality

--- src/addon.js
+++ src/addon.js
@@ -1,6 +1,18 @@
 (function(DriversMarket, TechnicalDirectorsMarket, RaceAnalysis, Qualify1, Qualify2, UpdateCar, RaceSetup, StaffAndFacilities) {
 	'use strict';
 
+	var URL = ['Qualify2', 'RaceAnalysis', 'Qualify', 'AvailTechDirectors', 'AvailDrivers', 'UpdateCar', 'RaceSetup', 'StaffAndFacilities'];
+	var ACTION = {
+		1: Qualify2.addFunctionality,
+		2: RaceAnalysis.addFunctionality,
+		3: Qualify1.addFunctionality,
+		4: TechnicalDirectorsMarket.addFunctionality,
+		5: DriversMarket.addFunctionality,
+		6: UpdateCar.addFunctionality,
+		7: RaceSetup.addFunctionality,
+		8: StaffAndFacilities.addFunctionality
+	};
+
 	var applesAddon = function() {
 		var pageType = checkPage(location.href);
 
@@ -11,71 +23,28 @@
 			var onlineInfo = $('#managersonline');
 
 			if (onlineInfo) {
-				onlineInfo.prepend('<li><a href="#" onclick="window.apples.forum.sendData()">Send data</a></li>');
+				onlineInfo.prepend('<li><a href="#" class="send-data" onclick="window.apples.forum.sendData()">Send data</a></li>');
 				$('body').append('<div style="position:absolute;top:0;bottom:0;left:0;right:0;display:none;z-index:9998;background-color:rgba(190,190,190,0.4);" class="overlay"></div><div style="position:absolute;left:45%;top:25%;display:none;z-index:9999;font-weight:bold;font-size:35px;color:#0f0;" class="overlay-message">Sending data...</div>');
 			}
 		}
 
-		// add functionality
-		switch (pageType) {
-			case 1: {
-				Qualify2.addFunctionality();
-				break;
-			}
-			case 2: {
-				RaceAnalysis.addFunctionality();
-				break;
-			}
-			case 3: {
-				Qualify1.addFunctionality();
-				break;
-			}
-			case 4: {
-				TechnicalDirectorsMarket.addFunctionality();
-				break;
-			}
-			case 5: {
-				DriversMarket.addFunctionality();
-				break;
-			}
-			case 6: {
-				UpdateCar.addFunctionality();
-				break;
-			}
-			case 7: {
-				RaceSetup.addFunctionality();
-				break;
-			}
-			case 8: {
-				StaffAndFacilities.addFunctionality();
-				break;
-			}
-			default:
-				break;
+		if (pageType) {
+			ACTION[pageType]();
 		}
 
 		function checkPage(url) {
-			var retval = 0;
-			
-			if (~url.indexOf('Qualify2')) {
-				retval = 1;
-			} else if (~url.indexOf('RaceAnalysis')) {
-				retval = 2;
-			} else if (~url.indexOf('Qualify')) {
-				retval = 3;
-			} else if (~url.indexOf('AvailTechDirectors')) {
-				retval = 4;
-			} else if (~url.indexOf('AvailDrivers')) {
-				retval = 5;
-			} else if (~url.indexOf('UpdateCar')) {
-				retval = 6;
-			} else if (~url.indexOf('RaceSetup')) {
-				retval = 7;
-			} else if (~url.indexOf('StaffAndFacilities')) {
-				retval = 8;
-			}
+			var result = 0;
 
-			return retval;
+			URL.some(function(item, index) {
+				if (~url.indexOf(item)) {
+					result = index + 1;
+					return true;
+				}
+
+				return false;
+			});
+
+			return result;
 		}
 
 		function hideAdBlock() {
